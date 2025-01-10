a3ab9460a223ee073ece36beee2294c418638300
Matthew Cleghorn <mr.m.cleghorn@gmail.com>
2025-01-10 13:39:16 +0000

    Add admin for shop

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Product, Category
 
 # Register your models here.
+
+class ShopAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'image',
+        'in_stock',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'display_name',
+        'name',
+    )
+
+
+admin.site.register(Product,  ShopAdmin)
+admin.site.register(Category, CategoryAdmin)
