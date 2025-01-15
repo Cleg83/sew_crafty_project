88f2af9c0b209cce6844fde5da6842248328afac
Matthew Cleghorn <mr.m.cleghorn@gmail.com>
2025-01-15 16:58:10 +0000

    Create checkout admin

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Order, LineItem
 
 # Register your models here.
+class OrderItemAdminInline(admin.TabularInline):
+    model = LineItem
+    readonly_fields = ('item_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderItemAdminInline,)
+
+    readonly_fields = ('order_number', 'date', 'item_count',
+                       'delivery_fee', 'order_total',
+                       'grand_total', 'original_basket', 'stripe_pid',)
+    
+    fields = ('order_number', 'user_profile', 'date', 'first_name', 
+              'last_name', 'email', 'phone_number', 'country', 
+              'postcode', 'town', 'address_1', 
+              'address_2', 'county', 'item_count', 
+              'delivery_fee', 'order_total', 'grand_total',
+              'original_basket', 'stripe_pid',)
+    
+    list_display = ('order_number', 'date', 'first_name',
+                    'last_name', 'order_total', 'delivery_fee',
+                    'grand_total',)
+    
+    ordering = ('-date',)
+
+
+admin.site.register(Order, OrderAdmin)
