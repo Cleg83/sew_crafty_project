b995f5ef026d283064901aff1c9dcb8edb6eb477
Matthew Cleghorn <mr.m.cleghorn@gmail.com>
2025-01-15 12:15:59 +0000

    Add cateogry filtering and sorting options to shop

--- shop/views.py
+++ shop/views.py
@@ -1,31 +1,51 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from .models import Product, Category
 
-from .models import Product
-
-# Create your views here.
 def shop_items(request):
     """ Shop view to show all items in the shop """
 
-    # View to display all shop items and provide search functionality
-
     shop_items = Product.objects.all()
     query = None
+    category_filter = None
+    sort_by = None
 
-    if request.GET:
-        if 'q' in request.GET:
-            query = request.GET['q']
-        if not query:
+    # Search functionality
+    if 'q' in request.GET:
+        query = request.GET['q']
+        if query:
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            shop_items = shop_items.filter(queries)
+        else:
             messages.error(request, 'Please enter some search criteria!')
-            return redirect(reverse('shop'))
 
-        queries = Q(name__icontains=query) | Q(description__icontains=query)
-        shop_items = shop_items.filter(queries)
+    # Category filtering
+    if 'category' in request.GET:
+        category_filter = request.GET['category']
+        if category_filter:
+            shop_items = shop_items.filter(category__id=category_filter)
+
+    # Sorting functionality
+    if 'sort' in request.GET:
+        sort_by = request.GET['sort']
+        if sort_by == 'price_low_to_high':
+            shop_items = shop_items.order_by('price')
+        elif sort_by == 'price_high_to_low':
+            shop_items = shop_items.order_by('-price')
+        elif sort_by == 'name_a_to_z':
+            shop_items = shop_items.order_by('name')
+        elif sort_by == 'name_z_to_a':
+            shop_items = shop_items.order_by('-name')
+
+    categories = Category.objects.all()
 
     context = {
         'shop_items': shop_items,
         'search_criteria': query,
+        'category_filter': category_filter,
+        'categories': categories,
+        'sort_by': sort_by,
         'on_shop_page': True,
     }
 
