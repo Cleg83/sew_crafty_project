7893f3426e181b50ebec557b05081a6852eae12f
Matthew Cleghorn <mr.m.cleghorn@gmail.com>
2025-01-15 16:55:55 +0000

    Add correct checkout and user_profile urls

--- sew_crafty/urls.py
+++ sew_crafty/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path('shop/', include('shop.urls')),
     path('basket/', include('basket.urls')),
     path('checkout/', include('checkout.urls')),
+    path('user_profile/', include('user_profile.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
