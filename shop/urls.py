e733eace774eb410b174505b6a40fbd274e8ff9d
Matthew Cleghorn <mr.m.cleghorn@gmail.com>
2025-01-17 17:54:46 +0000

    Add manage shop to urls

--- shop/urls.py
+++ shop/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.shop_items, name='shop'),
     path('<int:shop_item_id>/', views.shop_item_info, name='shop_item_info'),
+    path('manage_shop/', views.manage_shop, name='manage_shop'),
 ]
