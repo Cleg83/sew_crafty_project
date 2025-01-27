7ca8b9482102a228810151008c075144e9973543
Matthew Cleghorn <mr.m.cleghorn@gmail.com>
2025-01-27 14:07:15 +0000

    Add webhooks to urls

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from .webhooks import stripe_webhook
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('cache_checkout_data/', views.cache_checkout_data, name='cache_checkout_data'),
     path('success/<order_number>', views.success, name='success'),
+    path('webhook/', stripe_webhook, name='stripe_webhook'),
 ]
