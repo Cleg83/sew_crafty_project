135292b828829051e4597d5cbfe146a72a0bcafa
Matthew Cleghorn <mr.m.cleghorn@gmail.com>
2025-01-29 23:17:37 +0000

    Change to production DB, properly

--- sew_crafty/settings.py
+++ sew_crafty/settings.py
@@ -139,22 +139,19 @@ LOGOUT_REDIRECT_URL = '/'
 #     }
 # }
 
-DATABASES = {
-    'default': dj_database_url.parse(env.DATABASE_URL)
-}
 
-# if 'DATABASE_URL' in os.environ:
-#     DATABASES = {
-#         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     
-#     }
-# else:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': BASE_DIR / 'db.sqlite3',
-#         }
-#     }
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
