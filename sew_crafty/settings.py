db217e2a668adc3ecd0abd558021049e10817923
Matthew Cleghorn <mr.m.cleghorn@gmail.com>
2025-01-30 12:28:44 +0000

    Tweak debug settings for deployment

--- sew_crafty/settings.py
+++ sew_crafty/settings.py
@@ -226,5 +226,3 @@ STRIPE_ENDPOINT_SECRET = os.environ.get('STRIPE_ENDPOINT_SECRET', default='')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# END
