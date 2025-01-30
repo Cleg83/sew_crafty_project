7969fcea685ef66bb253acb00aea320fbf54639e
Matthew Cleghorn <mr.m.cleghorn@gmail.com>
2025-01-30 19:53:09 +0000

    Explicitly state debug=false to see if that fixes collectstatic not accessing custom_storages

--- sew_crafty/settings.py
+++ sew_crafty/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = ['sew-crafty-cleg83-7e1cea4bde60.herokuapp.com', 'localhost']
 
