b7512869443ceb1a05305fd6166c6fd8c6b39ab6
Matthew Cleghorn <mr.m.cleghorn@gmail.com>
2025-01-30 18:07:13 +0000

    Change STATICFILES_LOCATION endpoint

--- sew_crafty/settings.py
+++ sew_crafty/settings.py
@@ -198,6 +198,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 if 'USE_AWS' in os.environ:
+
+
     AWS_STORAGE_BUCKET_NAME = 'sew-crafty-cleg83'
     AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
@@ -205,9 +207,9 @@ if 'USE_AWS' in os.environ:
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
-    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_STORAGE = 'sew_crafty.custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
-    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    DEFAULT_FILE_STORAGE = 'sew_crafty.custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
 
     # Override static and media files in production
