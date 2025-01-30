dbdccef7d90fd236e1f219678fe097af05b0c8f6
Matthew Cleghorn <mr.m.cleghorn@gmail.com>
2025-01-30 20:52:50 +0000

    Change Static_Root to None to try and solve collectstatic issue

--- custom_storages.py
+++ custom_storages.py
@@ -5,10 +5,6 @@ from storages.backends.s3boto3 import S3Boto3Storage
 class StaticStorage(S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
 
-    def __init__(self, *args, **kwargs):
-        print("StaticStorage initialized!")  # Add this line for debugging
-        super().__init__(*args, **kwargs)
-
 
 class MediaStorage(S3Boto3Storage):
     location = settings.MEDIAFILES_LOCATION 
