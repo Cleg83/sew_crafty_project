6fac7c9b1546675a9da6c97a8a47d4a9f57edb76
Matthew Cleghorn <mr.m.cleghorn@gmail.com>
2025-02-04 11:24:49 +0000

    Change email provider from gmail to outlook

--- sew_crafty/settings.py
+++ sew_crafty/settings.py
@@ -238,7 +238,7 @@ if 'DEVELOPMENT' in os.environ:
     DEFAULT_FROM_EMAIL = 'sewcrafty@example.com'
 else: 
     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-    EMAIL_HOST = "smtp.gmail.com"
+    EMAIL_HOST = "smtp-mail.outlook.com"
     EMAIL_PORT = 587
     EMAIL_USE_TLS = True  
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
